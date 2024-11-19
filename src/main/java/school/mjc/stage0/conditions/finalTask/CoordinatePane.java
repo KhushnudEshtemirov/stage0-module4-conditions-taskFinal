f0688aabb6b07533a36ca04ddb087710049366cf
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-11-19 16:45:30 +0500

    Finished last tasks of module 4.1

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (0 < x && y > 0) {
+            System.out.println("first");
+        } else if (0 < x && y < 0) {
+            System.out.println("second");
+        } else if (0 > x && y < 0) {
+            System.out.println("third");
+        } else if (0 > x && y > 0) {
+            System.out.println("fourth");
+        } else {
+            System.out.println("zero");
+        }
     }
 }
